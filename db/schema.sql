7e77c053bb170f0c80a1ed7fe5afc8916c24a839
xiongxeng <xiongxeng@gmail.com>
2023-11-22 14:39:56 -0600

    db schema to create tables

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+use employee_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  CONSTRAINT fk_deparment FOREIGN KEY (department_id) REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id)
+  ON DELETE SET NULL,
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id)
+  ON DELETE SET NULL
+);
+
+
